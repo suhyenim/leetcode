10c83481091ce811cc46e8ca83f690c78bc78765
Suhyen Im <ishyenn@gmail.com>
2024-06-22 18:01:20 +0900

    Time: 30 ms (97.53%), Space: 16.6 MB (44.14%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,9 +1,10 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        while len(sandwiches) != 0 and sandwiches[0] in students:
-            if students[0] == sandwiches[0]:
-                students.pop(0)
-                sandwiches.pop(0)
+        dic1 = Counter(students)
+        dic2 = Counter(sandwiches)
+        for c in sandwiches:
+            if dic1[c] != 0:
+                dic1[c] -= 1
             else:
-                students.append(students.pop(0))
-        return len(students)
+                break
+        return sum(dic1.values())
