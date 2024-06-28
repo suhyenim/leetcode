dcc37066cdb56bb909b21d5f202aa1cf7b7cb94d
Suhyen Im <ishyenn@gmail.com>
2024-06-28 18:04:40 +0900

    Time: 36 ms (73.89%), Space: 16.5 MB (37.12%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
@@ -3,12 +3,5 @@ class Solution:
         ones_count = s.count('1')
         zeros_count = s.count('0')
         
-        if ones_count > 1:
-            result = '1' * (ones_count - 1) + '0' * zeros_count + '1'
-        elif ones_count == 1:
-            result = '0' * zeros_count + '1'
-        else:
-            result = s
-        
-        return result
+        return '1' * (ones_count - 1) + '0' * zeros_count + '1'
   
