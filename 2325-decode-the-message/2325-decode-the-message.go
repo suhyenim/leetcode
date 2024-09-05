106657024b749a41a452bcf02d9a49dd1eb9a3c0
Suhyen Im <suhyenim.kor@gmail.com>
2024-09-06 03:05:35 +0900

    Time: 43 ms (23.43%), Space: 16.6 MB (21.18%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.go
@@ -0,0 +1,16 @@
+class Solution:
+    def decodeMessage(self, key: str, message: str) -> str:
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        sub_table = {}
+        i = 0
+        for char in key:
+            if char != ' ' and char not in sub_table.keys():
+                sub_table[char] = alphabet[i] 
+                i += 1     
+        ans = []
+        for char in message:
+            if char == ' ':
+                ans.append(' ')
+            else:
+                ans.append(sub_table[char])
+        return ''.join(ans);
