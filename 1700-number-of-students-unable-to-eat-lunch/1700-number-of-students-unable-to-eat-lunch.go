03689412eb490f3649f197a54f0c1d2d76c34add
Suhyen Im <suhyenim.kor@gmail.com>
2024-09-06 03:34:57 +0900

    Time: 0 ms (100.00%), Space: 2.4 MB (82.72%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.go
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.go
@@ -1,6 +1,14 @@
 func countStudents(students []int, sandwiches []int) int {
-    tmp, n, i := [2]int{}, len(students), 0
-    for _, v := range students { tmp[v]++ }
-    for i < n && tmp[sandwiches[i]] > 0 { tmp[sandwiches[i]]--; i++ }
-    return n - i
+    passed := 0
+    for len(students) != 0 && passed != len(students) {
+        if students[0] != sandwiches[0] {
+            passed++
+            students = append(students[1:], students[0])
+        } else {
+            passed = 0
+            students = students[1:]
+            sandwiches = sandwiches[1:]
+        }
+    }
+    return passed
 }
